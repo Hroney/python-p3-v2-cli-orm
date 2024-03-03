f3b01190b0343c33149da8a9e73c6e22769060b4
Hayden Roney <roneyhayden@gmail.com>
2024-03-03 11:20:17 -0600

    lab done

--- lib/helpers.py
+++ lib/helpers.py
@@ -10,27 +10,71 @@ def exit_program():
 
 
 def list_departments():
-    pass
+    print('\n')
+    departments = Department.get_all()
+    for department in departments:
+        print(department)
+    print('\n')
 
 
 def find_department_by_name():
-    pass
+    print('\n')
+    name = input("Enter the department's name: ")
+    department = Department.find_by_name(name)
+    print(department) if department else print (f'Department "{name}" not found.')
+    print('\n')
 
 
 def find_department_by_id():
-    pass
+    print('\n')
+    id_ = input("Enter the department's id: ")
+    department = Department.find_by_id(id_)
+    print(department) if department else print(f'Department "{id_}" not found.')
+    print('\n')
 
 
 def create_department():
-    pass
+    print('\n')
+    name = input("Enter the department's name: ")
+    location = input("Enter the department's location: ")
+    try:
+        department = Department.create(name, location)
+        print(f'Success: {department}')
+    except Exception as exc:
+        print("Error creating department: ", exc)
+    print('\n')
+    
 
 
 def update_department():
-    pass
+    print('\n')
+    id_ = input("Enter the department's id: ")
+    if department := Department.find_by_id(id_):
+        try:
+            name = input("Enter the department's new name: ")
+            department.name = name
+            location = input("Enter the department's new location: ")
+            department.location = location
+            
+            department.update()
+            print(f'Success: {department}')
+        except Exception as exc:
+            print("Error updating department: ", exc)
+    else:
+        print(f'Department "{id_}" not found')
+    print('\n')
 
 
 def delete_department():
-    pass
+    print('\n')
+    id_ = input("Enter the department's id: ")
+    if department := Department.find_by_id(id_):
+        department.delete()
+        print(f'Department {id_} deleted')
+    else:
+        print(f'Department {id_} not found')
+    print('\n')
+
 
 
 # You'll implement the employee functions in the lab
